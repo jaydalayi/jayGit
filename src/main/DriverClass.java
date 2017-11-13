853e37e4c203471859caac9cf643fff2e0e73992
jaydalayi <jaydalayi@172.16.80.56>
2017-11-13 15:43:57 -0800

    sample class testing

---
+++ src/main/DriverClass.java
@@ -0,0 +1,12 @@
+package main;
+
+public class DriverClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome baby!");
+
+	}
+
+}
